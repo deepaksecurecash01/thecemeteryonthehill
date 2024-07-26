d647878e9cf06661a37f6d3f18023196b4bdf0d3
deepaksecurecash01 <deepak@securecash.com.au>
2024-07-26 15:37:26 +0930

    Stripe added

--- src/app/api/create-payment-intent/route.js
+++ src/app/api/create-payment-intent/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY_TEST);
+const stripe = require("stripe")(process.env.STRIPE_KEY_TEST);
 
 export async function POST(request) {
   try {
